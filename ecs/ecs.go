9808f7e01663c5c3dcab97fe72d0c0af64574ec3
thisisaaronland <devnull@localhost>
2019-02-13 10:20:54 -0800

    move the guts of things in to ecs/ecs.go; compiles but untested

--- ecs/ecs.go
+++ ecs/ecs.go
@@ -1,11 +1,19 @@
 package ecs
 
 import (
+	"errors"
+	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
-	aws_cloudwatch "github.com/aws/aws-sdk-go/service/cloudwatchlogs"
+	aws_cloudwatchlogs "github.com/aws/aws-sdk-go/service/cloudwatchlogs"
 	aws_ecs "github.com/aws/aws-sdk-go/service/ecs"
 	"github.com/whosonfirst/go-whosonfirst-aws/session"
+	"log"
+	"strings"
 )
+
+type TaskResponse struct {
+	Tasks []string
+}
 
 type TaskOptions struct {
 	DSN            string
@@ -16,15 +24,114 @@ type TaskOptions struct {
 	PublicIP       string
 	Subnets        []string
 	SecurityGroups []string
+	Monitor        bool
+	MonitorDSN     string
 }
 
-type MonitorOptions struct {
-	DSN string
+func LaunchTask(task_opts *TaskOptions, cmd ...string) (*aws_ecs.RunTaskOutput, error) {
+
+	ecs_sess, err := session.NewSessionWithDSN(task_opts.DSN)
+
+	if err != nil {
+		return nil, err
+	}
+
+	ecs_svc := aws_ecs.New(ecs_sess)
+
+	cluster := aws.String(task_opts.Cluster)
+	task := aws.String(task_opts.Task)
+
+	launch_type := aws.String(task_opts.LaunchType)
+	public_ip := aws.String(task_opts.PublicIP)
+
+	subnets := make([]*string, len(task_opts.Subnets))
+	security_groups := make([]*string, len(task_opts.SecurityGroups))
+
+	for i, sn := range task_opts.Subnets {
+		subnets[i] = aws.String(sn)
+	}
+
+	for i, sg := range task_opts.SecurityGroups {
+		security_groups[i] = aws.String(sg)
+	}
+
+	aws_cmd := make([]*string, len(cmd))
+
+	for i, str := range cmd {
+		aws_cmd[i] = aws.String(str)
+	}
+
+	network := &aws_ecs.NetworkConfiguration{
+		AwsvpcConfiguration: &aws_ecs.AwsVpcConfiguration{
+			AssignPublicIp: public_ip,
+			SecurityGroups: security_groups,
+			Subnets:        subnets,
+		},
+	}
+
+	process_override := &aws_ecs.ContainerOverride{
+		Name:    aws.String(task_opts.Container),
+		Command: aws_cmd,
+	}
+
+	overrides := &aws_ecs.TaskOverride{
+		ContainerOverrides: []*aws_ecs.ContainerOverride{
+			process_override,
+		},
+	}
+
+	input := &aws_ecs.RunTaskInput{
+		Cluster:              cluster,
+		TaskDefinition:       task,
+		LaunchType:           launch_type,
+		NetworkConfiguration: network,
+		Overrides:            overrides,
+	}
+
+	task_rsp, err := ecs_svc.RunTask(input)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(task_rsp.Tasks) == 0 {
+		return nil, errors.New("run task returned no errors... but no tasks")
+	}
+
+	if task_opts.Monitor {
+
+		task_arns := make([]string, len(task_rsp.Tasks))
+
+		for i, t := range task_rsp.Tasks {
+			task_arns[i] = *t.TaskArn
+		}
+
+		err := MonitorTasksWithECSService(ecs_svc, task_opts, task_arns...)
+
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return task_rsp, nil
 }
 
-func LaunchTaskAndMonitor(ecs_opts *TaskOptions, cw_opts *MonitorOptions, cmd ...string) error {
+func MonitorTasks(task_opts *TaskOptions, task_arns ...string) error {
 
-	cw_sess, err := session.NewSessionWithDSN(cw_opts.DSN)
+	ecs_sess, err := session.NewSessionWithDSN(task_opts.DSN)
+
+	if err != nil {
+		return err
+	}
+
+	ecs_svc := aws_ecs.New(ecs_sess)
+
+	return MonitorTasksWithECSService(ecs_svc, task_opts, task_arns...)
+}
+
+func MonitorTasksWithECSService(ecs_svc *aws_ecs.ECS, task_opts *TaskOptions, task_arns ...string) error {
+
+	cw_sess, err := session.NewSessionWithDSN(task_opts.MonitorDSN)
 
 	if err != nil {
 		return err
@@ -32,27 +139,21 @@ func LaunchTaskAndMonitor(ecs_opts *TaskOptions, cw_opts *MonitorOptions, cmd ..
 
 	cw_svc := aws_cloudwatchlogs.New(cw_sess)
 
-	rsp, err := LaunchTask(ecs_opts*TaskOptions, cmd...)
-
-	if err != nil {
-		return err
-	}
-
-	count_tasks := len(rsp.Tasks)
+	count_tasks := len(task_arns)
 	remaining := count_tasks
 
 	ecs_tasks := make([]*string, count_tasks)
 
-	for i, t := range rsp.Tasks {
-		ecs_tasks[i] = t.TaskArn
+	for i, t := range task_arns {
+		ecs_tasks[i] = aws.String(t)
 	}
 
 	task_errors := make([]error, 0)
 
 	for remaining > 0 {
 
-		monitor_req := &ecs.DescribeTasksInput{
-			Cluster: aws.String(ecs_opts.Cluster),
+		monitor_req := &aws_ecs.DescribeTasksInput{
+			Cluster: aws.String(task_opts.Cluster),
 			Tasks:   ecs_tasks,
 		}
 
@@ -66,7 +167,7 @@ func LaunchTaskAndMonitor(ecs_opts *TaskOptions, cw_opts *MonitorOptions, cmd ..
 
 			for _, c := range t.Containers {
 
-				if *c.Name != ecs_opts.Container {
+				if *c.Name != task_opts.Container {
 					continue
 				}
 
@@ -79,10 +180,10 @@ func LaunchTaskAndMonitor(ecs_opts *TaskOptions, cw_opts *MonitorOptions, cmd ..
 
 				arn := strings.Split(*t.TaskArn, "/")
 
-				cw_group := fmt.Sprintf("/ecs/%s", opts.Container)
-				cw_stream := fmt.Sprintf("ecs/%s/%s", opts.Container, arn[1])
+				cw_group := fmt.Sprintf("/ecs/%s", task_opts.Container)
+				cw_stream := fmt.Sprintf("ecs/%s/%s", task_opts.Container, arn[1])
 
-				cw_req := &cloudwatchlogs.GetLogEventsInput{
+				cw_req := &aws_cloudwatchlogs.GetLogEventsInput{
 					LogGroupName:  aws.String(cw_group),
 					LogStreamName: aws.String(cw_stream),
 					StartFromHead: aws.Bool(true),
@@ -120,77 +221,4 @@ func LaunchTaskAndMonitor(ecs_opts *TaskOptions, cw_opts *MonitorOptions, cmd ..
 	}
 
 	return nil
-}
-
-func LaunchTask(opts *TaskOptions, cmd ...string) (*aws_ecs.RunTaskOutput, error) {
-
-	ecs_sess, err := session.NewSessionWithDSN(opts.DSN)
-
-	if err != nil {
-		return nil, err
-	}
-
-	ecs_svc := aws_ecs.New(ecs_sess)
-
-	cluster := aws.String(opts.Cluster)
-	task := aws.String(opts.Task)
-
-	launch_type := aws.String(opts.LaunchType)
-	public_ip := aws.String(opts.PublicIP)
-
-	subnets := make([]*string, len(opts.Subnets))
-	security_groups := make([]*string, len(opts.SecurityGroups))
-
-	for i, sn := range opts.Subnets {
-		subnets[i] = aws.String(sn)
-	}
-
-	for i, sg := range opts.SecurityGroups {
-		security_groups[i] = aws.String(sg)
-	}
-
-	aws_cmd := make([]*string, len(cmd))
-
-	for i, str := range cmd {
-		aws_cmd[i] = aws.String(str)
-	}
-
-	network := &aws_ecs.NetworkConfiguration{
-		AwsvpcConfiguration: &aws_ecs.AwsVpcConfiguration{
-			AssignPublicIp: public_ip,
-			SecurityGroups: security_groups,
-			Subnets:        subnets,
-		},
-	}
-
-	process_override := &aws_ecs.ContainerOverride{
-		Name:    aws.String(opts.Container),
-		Command: aws_cmd,
-	}
-
-	overrides := &aws_ecs.TaskOverride{
-		ContainerOverrides: []*ecs.ContainerOverride{
-			process_override,
-		},
-	}
-
-	input := &aws_ecs.RunTaskInput{
-		Cluster:              cluster,
-		TaskDefinition:       task,
-		LaunchType:           launch_type,
-		NetworkConfiguration: network,
-		Overrides:            overrides,
-	}
-
-	rsp, err := ecs_svc.RunTask(input)
-
-	if err != nil {
-		return nil, err
-	}
-
-	if len(rsp.Tasks) == 0 {
-		return nil, errors.New("run task returned no errors... but no tasks")
-	}
-
-	return rsp, nil
 }
